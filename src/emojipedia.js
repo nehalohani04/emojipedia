b7d03acba921b888efc5196eb6ca8559f8bf7cfd
Neha Lohani <nehalohani004@gmail.com>
2023-07-25 23:06:40 +0530

    emojipedia

---
+++ src/emojipedia.js
@@ -0,0 +1,47 @@
+const emojipedia = [
+    {
+      id: 1,
+      emoji: "😇",
+      name: "Smiling Face With Halo",
+      meaning:
+       "A heavenly smile is shown by the 😇 halo emoji, which symbolizes innocence and goodness. People use it to express their good deeds, loving behavior or exemplary attitude"
+    },
+    {
+      id: 2,
+      emoji: "😋",
+      name: "Face Savouring Delicious Food",
+      meaning:
+       "Bon appetit! Licking your lips with pleasure, the 😋 emoji shows an expression of satisfaction, hunger or joy over delicious food. It is shown beaming with joy when you have cooked something particularly tasty or when your plate is empty."
+    },
+    {
+      id: 3,
+      emoji: "🤣",
+      name: "Rolling On The Floor, Laughing",
+      meaning:
+        "That's funny! Expressions of pure hilarity are conveyed by the 🤣 emoji, which represents a person rolling on the floor with laughter. It symbolizes that something is so incredibly funny that you can barely contain your laughter."
+    },
+    {
+      id: 4,
+      emoji: "🤓",
+      name: "Nerd Face",
+      meaning:
+        "Huge glasses, awkward smile and buck teeth. Used humorously or ironically for nerds or to express how smart you are. Stereotype of a nerd; a smart but funny-dressed person with social deficits."
+    },
+    {
+      id: 5,
+      emoji: "🥲",
+      name:"Smiling face with tear",
+      meaning:
+     "The 🥲 emoji stands for smiling through the tough times and symbolizes trying to stay positive even in difficult moments. You are smiling through the pain instead of being truly happy. It shows the ability to hide true feelings behind a smile when dealing with problems or difficult times in life."
+    },
+    {
+      id: 6,
+      emoji: "🙃",
+      name:"Upside-Down Face",
+      meaning:
+      "I'm not serious! The inverted smiling face 🙃 indicates that you are not serious or have a humorous undertone. It is suitable for ambiguous statements, ironic remarks or joking comments. Also, it can be used to express silliness or in response to a joke. In conversations, it emphasizes the playful nature of a message and helps clarify potential misunderstandings about the tone of the message. "
+    }
+  ];
+  
+  export default emojipedia;
+  
